6ae295ff9c1f3d4d46ea2dee35016037bc06c45b
duc.nguyen97 <duc.nguyen@quant-edge.com>
2019-06-13 11:29:08 +0700

    add terms of service and private policy pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,19 @@ import './Footer.css';
 function Footer() {
   return (
     <footer>
+      <section>
+        <span>
+          <a href="/terms-of-service" target="_blank">
+            Terms of Service
+          </a>
+        </span>
+        <span> | </span>
+        <span>
+          <a href="/private-policy" target="_blank">
+            Private Policy
+          </a>
+        </span>
+      </section>
       <section className="author">
         <p>2019 @vietduc01100001</p>
       </section>
